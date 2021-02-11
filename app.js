d4b60ea4e4db8f71606a70ad9362d0444fe3998a
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-11 21:43:08 +0000

    Updating Routes

--- app.js
+++ app.js
@@ -150,27 +150,27 @@ const deleteUser = (req, res) => {
 };
 
 // 3) Routes
-app
-  .route('/api/v1/tours')
-  .get(getAllTours)
-  .post(createTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-  .route('/api/v1/tours/:id')
+tourRouter.route('/').get(getAllTours).post(createTour);
+
+tourRouter
+  .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
 
-app
-  .route('/api/v1/users')
-  .get(getAllUsers)
-  .post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-app
-  .route('/api/v1/users/:id')
+userRouter
+  .route('/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4) Start Server
 const port = 3000;
